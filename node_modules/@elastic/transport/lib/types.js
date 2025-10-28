173b2fd7e4b4441cc26055d25bf0a1cd00be10d4
Gautami Jambotkar <gautamijambotkar@Gautamis-MacBook-Air.local>
2025-10-29 00:33:07 +0530

    Add .gitignore and remove node_modules from git

--- node_modules/@elastic/transport/lib/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/*
- * Copyright Elasticsearch B.V. and contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
